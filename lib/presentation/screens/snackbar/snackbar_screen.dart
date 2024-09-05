14d5d39584e3a03bc11e7c4ed6b8e5038f2b217d
Fabio Fonseca <fabiofonseca.arg@gmail.com>
2024-09-04 23:16:04 -0500

    dialogs added

--- lib/presentation/screens/snackbar/snackbar_screen.dart
+++ lib/presentation/screens/snackbar/snackbar_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'package:go_router/go_router.dart';
 
 class SnackbarScreen extends StatelessWidget {
 
@@ -24,11 +24,54 @@ class SnackbarScreen extends StatelessWidget {
     ScaffoldMessenger.of(context).showSnackBar(snackbar);
   }
 
+  void openDialog(BuildContext context) {
+    showDialog(
+      context: context, 
+      barrierDismissible: false,
+      builder: (context) => AlertDialog(
+        title: const Text('Are you sure?'),
+        content: const Text('Elit velit sunt aliqua proident ut quis enim enim. Consectetur ut consequat minim magna ad nisi fugiat Lorem incididunt adipisicing mollit id.'),
+        actions: [
+          TextButton(
+            onPressed: () => context.pop(), 
+            child: const Text('Cancel')
+          ),
+          FilledButton(
+            onPressed: () => context.pop(), 
+            child: const Text('Accept')
+          )
+        ],
+      )
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Snackbars y Diálogos'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment:  MainAxisAlignment.center,
+          children: [
+            FilledButton(
+              child: const Text('Licencias usadas'),
+              onPressed: (){
+                showAboutDialog(
+                  context: context,
+                  children: [
+                    const Text('Incididunt proident excepteur cillum labore laborum in qui pariatur pariatur.')
+                  ]
+                  );
+              },
+            ),
+            FilledButton(
+              child: const Text('Mostrar Diálogo'),
+              onPressed: () => openDialog(context),
+            ),
+          ],
+        ),
       ),
       floatingActionButton: FloatingActionButton.extended(
         label: const Text('Mostrar Snackbar'),
